3f976869abe9001d149d5fb967b41186db52431c
Yoki Yu <yokiyuqian@gmail.com>
2021-12-16 11:20:54 +0800

    feat: add semantic-release, complete config

---
+++ release.config.js
@@ -0,0 +1,77 @@
+/** @type {import('semantic-release').Options} */
+const config = {
+  branches: 'main',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        releaseRules: [
+          {type: 'chore', release: 'patch'},
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'build',
+            release: 'patch',
+          },
+          {
+            type: 'ci',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+          {
+            type: 'test',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+        presetConfig: {
+          types: [
+            {type: 'feat', section: 'Features'},
+            {type: 'fix', section: 'Bug Fixes'},
+            {type: 'chore', hidden: false},
+            {type: 'build', hidden: false},
+            {type: 'ci', hidden: false},
+            {type: 'docs', hidden: false},
+            {type: 'style', hidden: false},
+            {type: 'refactor', hidden: false},
+            {type: 'perf', hidden: false},
+            {type: 'test', hidden: false},
+          ],
+        },
+      },
+    ],
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/changelog',
+  ],
+};
+
+module.exports = config;
