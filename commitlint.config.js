f204db427c52aa08703abc2408e2b911edda650e
Yoki Yu <yokiyuqian@gmail.com>
2022-02-18 17:59:09 +0800

    refactor: config files, add qhlab eslint plugin

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'body-max-line-length': [0, 'always', 'Infinity'],
-    'footer-max-line-length': [0, 'always', 'Infinity'],
-  },
-};
